3e2e47e695144131cd55f1d7069814b2e59e06f9
Dhani-1428 <140617643+Sheetallinks@users.noreply.github.com>
2025-12-31 13:18:07 +0000

    Remove unused printReceiptWithLanguageSelection import

--- components/search-repair-tickets.tsx
+++ components/search-repair-tickets.tsx
@@ -26,7 +26,6 @@ import { useTranslation } from "@/components/language-provider"
 import { toast } from "sonner"
 import { getUserData, setUserData } from "@/lib/storage"
 import { exportToCSV } from "@/lib/export-utils"
-import { printReceiptWithLanguageSelection } from "@/components/new-repair-ticket-form"
 
 type SearchRepairTicketsProps = {
   initialStatusFilter?: string
